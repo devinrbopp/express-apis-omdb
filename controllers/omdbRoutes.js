99cd7d6a06553924388acf39d288d73d576b1422
devinrbopp <devinrbopp@gmail.com>
2021-11-08 10:28:25 -0700

    working search with results and details pages

---
+++ controllers/omdbRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+
+// this is our results route aka controller
+router.get('/results', function(req, res) {
+    let movieTitle = req.query.movieTitle
+
+    axios.get(`http://www.omdbapi.com/?s=${movieTitle}&apikey=${process.env.OMBD_API_KEY}`)
+        .then(apiResults => {
+            // console.log(apiResults.data.Search)
+            const results = apiResults.data.Search
+            res.render('results.ejs', {results})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+// we want to make this a real index, so we'll use a different axios call
+// we'll also use a different route to hit for that
+// finally, we'll render a different page
+router.get('/:movie_id', function(req, res) {
+    // this console.log displays our request object
+    // console.log('this is req.query',req.query)
+    let imdbId = req.params.movie_id
+    console.log('this is the movie title:', imdbId)
+    // now we can use the movie title to build the request url, and make the call with axios
+    axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMBD_API_KEY}&i=${imdbId}`)
+      .then(apiRes => {
+        console.log('this is apiRes.data', apiRes.data)
+        let title = apiRes.data.Title
+        let year = apiRes.data.Year
+        let plot = apiRes.data.Plot
+        let imdbID = apiRes.data.imdbID
+        // res.render results to results.ejs, with our selected data sent as an object
+        res.render('detail.ejs', {title, year, plot, imdbID})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios')
 const app = express();
 
 // Sets EJS as the view engine
@@ -15,13 +16,46 @@ app.use(ejsLayouts);
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
 
-// Routes
+// Routes are also know as controllers
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  // this .send works well for a little message but not for web content
+  // res.send('Hello, backend!');
+  // res.render serves up render.ejs from the views folder
+  res.render('index')
 });
 
+// THIS WAS MOVED TO OUR ROUTERS (CONTROLLERS/OMDBROUTES.JS) DIRECTORY
+// // this is our results route aka controller
+// app.get('/results', function(req, res) {
+//   // this console.log displays our request object
+//   // console.log('this is req.query',req.query)
+//   let movieTitle = req.query.movieTitle
+//   console.log('this is the movie title:', movieTitle)
+//   // now we can use the movie title to build the request url, and make the call with axios
+//   axios.get(`http://www.omdbapi.com/?t=${movieTitle}&apikey=${process.env.OMBD_API_KEY}`)
+//     .then(apiRes => {
+//       console.log('this is apiRes.data', apiRes.data)
+//       let title = apiRes.data.Title
+//       let year = apiRes.data.Year
+//       let plot = apiRes.data.Plot
+//       let imdbID = apiRes.data.imdbID
+//       // res.render results to results.ejs, with our selected data sent as an object
+//       res.render('results', {title, year, plot, imdbID})
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+// })
+
+app.use('/movies', require('./controllers/omdbRoutes.js'))
+
+// sets our entry point to run our app/server (same thing in this case)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`listening to the sweet sounds ${process.env.PORT}. time to make some requests.`)
+})
+
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+// var server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
-module.exports = server;
+// module.exports = server;
